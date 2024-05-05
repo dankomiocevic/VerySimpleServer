0ce1014c3dc86dd3b46c20de1916bfde58b8d10b
Danko Miocevic <dankomiocevic@gmail.com>
2024-05-05 13:22:28 -0700

    Add tests

---
+++ config_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestConfigureSlot(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	slot_one := make(map[string]interface{})
+
+	slot_one["kind"] = "simple_memory"
+	config["slot_000"] = slot_one
+
+	response := configureSlots(config)
+
+	if response[0] == nil {
+		t.Fatalf("slot zero not configured: %s", response)
+	}
+}
+
+func TestNotConfigureSlot(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	slot_one := make(map[string]interface{})
+
+	slot_one["kind"] = "simple_memory"
+	config["slot_000"] = slot_one
+
+	response := configureSlots(config)
+
+	for i := 1; i < 1000; i++ {
+		if response[i] != nil {
+			t.Fatalf("slot %d should not be configured: %s", i, response)
+		}
+	}
+}
+
+func TestConfigureUnknownType(t *testing.T) {
+	config := make(map[interface{}]interface{})
+	slot_one := make(map[string]interface{})
+
+	slot_one["kind"] = "unknown"
+	config["slot_000"] = slot_one
+
+	response := configureSlots(config)
+
+	if response[0] != nil {
+		t.Fatalf("slot zero should not be configured: %s", response)
+	}
+}
